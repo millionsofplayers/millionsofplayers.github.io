3c276c900fceb7b9c36f4f338d0f9b7b2ad50e87
millions <ofplayers@proton.me>
2024-03-17 11:03:04 -0900

    commented out twitter link on /old

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-var quotes = ['Why do they call it oven when you of in the cold food of out hot eat the food?', 'Toasters. That\'s it, that\'s the quote.', 'huhu yo', 'nah, i\'d win.', '86', 'Remember: ', 'DOMAIN EXPANSION SHITTY WEBSITE', 'want to see more quotes? refresh the page or wait for me to update this'];
+var quotes = ['Why do they call it oven when you of in the cold food of out hot eat the food?', 'Toasters. That\'s it, that\'s the quote.', 'huhu yo', 'nah, i\'d win.', '86', 'Remember: ', 'DOMAIN EXPANSION SHITTY WEBSITE', 'want to see more quotes? refresh the page or wait for me to update this', 'People die when they are killed.'];
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
